40caa5cdfb72187733b32ab8ad7fd5e1e2b47337
emiraksoydan <emir02_03@hotmail.com>
2026-04-04 01:13:10 +0300

    force buildTools 35.0.0 to fix AAPT2 bug in v36

--- plugins/withGradleProperties.js
+++ plugins/withGradleProperties.js
@@ -4,16 +4,18 @@ const withDisableResourceOptimizations = (config) => {
   return withGradleProperties(config, (mod) => {
     const props = mod.modResults;
 
-    const alreadySet = props.some(
-      (p) => p.type === 'property' && p.key === 'android.enableResourceOptimizations'
-    );
+    const entries = [
+      { key: 'android.enableResourceOptimizations', value: 'false' },
+      { key: 'android.buildToolsVersion', value: '35.0.0' },
+    ];
 
-    if (!alreadySet) {
-      props.push({
-        type: 'property',
-        key: 'android.enableResourceOptimizations',
-        value: 'false',
-      });
+    for (const entry of entries) {
+      const alreadySet = props.some(
+        (p) => p.type === 'property' && p.key === entry.key
+      );
+      if (!alreadySet) {
+        props.push({ type: 'property', key: entry.key, value: entry.value });
+      }
     }
 
     return mod;
