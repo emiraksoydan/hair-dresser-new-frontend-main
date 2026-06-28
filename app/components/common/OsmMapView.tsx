c67548b35f6468c361eb78f29e27ddcb784487a6
emiraksoydan <emir02_03@hotmail.com>
2026-06-28 23:59:26 +0300

    last-commit

--- app/components/common/OsmMapView.tsx
+++ app/components/common/OsmMapView.tsx
@@ -15,6 +15,14 @@ export const OsmMapView = React.forwardRef<MapView, OsmMapViewProps>(
   ({ children, userInterfaceStyle, ...props }, ref) => {
     const isAndroid = Platform.OS === "android";
 
+    // New Architecture (Fabric) altinda react-native-maps'in legacy interop'u,
+    // children arasinda null/false/undefined oldugunda native tarafta nil subview
+    // mount edip "object cannot be nil" SIGABRT'ine yol acabiliyor. Falsy cocuklari
+    // burada eleyerek haritaya yalnizca gercek React elementleri gondeririz.
+    const safeChildren = React.Children.toArray(children).filter(
+      (child) => React.isValidElement(child)
+    );
+
     return (
       <MapView
         ref={ref}
@@ -23,7 +31,7 @@ export const OsmMapView = React.forwardRef<MapView, OsmMapViewProps>(
         userInterfaceStyle={isAndroid ? undefined : userInterfaceStyle}
         {...props}
       >
-        {children}
+        {safeChildren}
       </MapView>
     );
   }
